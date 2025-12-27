1df261bdeb5b5b7ec834299dacf67eda5e0aa8e5
sparklink-ts <sparklink.ts@gmail.com>
2025-12-27 21:18:55 +0530

    AWS SpringBoot Example : Base commit

---
+++ src/main/java/com/springbootawsdemo/SpringBootAWSDemoApplication.java
@@ -0,0 +1,12 @@
+package com.springbootawsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAWSDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAWSDemoApplication.class, args);
+	}
+}
